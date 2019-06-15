a8e9f0a1c5f88238c016f84106e5fb744291da5f
bigbwalter <bigbwalter@gmail.com>
2019-06-15 01:24:34 -0600

    Create Problem1.java

---
+++ JavaSolutions/Problem1.java
@@ -0,0 +1,14 @@
+// Created by Brian Walter
+
+public class Problem1{
+
+	public static void main(String[] args){
+		int total = 0;
+		for(int i = 1; i < 1000; i++){
+			if(i%3 == 0 || i%5 == 0)
+				total += i;
+		}
+		System.out.println(total);
+	}
+	
+}
